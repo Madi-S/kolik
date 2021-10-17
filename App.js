2ea0fc0f2d00ae94009133b38e59c9557285cacf
Madi-S <khovansky99@gmail.com>
2021-10-17 14:35:28 +0600

    AppLoading implemented (fonts + theme)

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import AppLoading from 'expo-app-loading'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+import THEME from './src/theme'
+import loadApp from './src/loadApp'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    console.log('Rendered app')
+    const [isLoading, setIsLoading] = useState(true)
+
+    if (isLoading) {
+        return (
+            <AppLoading
+                startAsync={loadApp}
+                onFinish={() => setIsLoading(false)}
+                onError={err => console.warn('Failed to load app', err)}
+            />
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Testing fonts</Text>
+            <StatusBar style='auto' />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: THEME.SECONDARY_COLOR,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontFamily: 'm-bold',
+        color: THEME.PRIMARY_COLOR
+    }
+})
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+const THEME = {
+    INFO_COLOR: '#457b9d',
+    PRIMARY_COLOR: '#1d3557',
+    DANGER_COLOR: '#e63946',
+    SECONDARY_COLOR: '#a8dadc',
+}
+
+export default THEME
