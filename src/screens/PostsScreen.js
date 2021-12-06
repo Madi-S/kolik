23804d03ad446a79883f89a7c8c37e9cd111cb84
Madi-S <khovansky99@gmail.com>
2021-12-06 20:08:56 +0600

    Posts now returns post detail screen

--- src/screens/PostsScreen.js
+++ src/screens/PostsScreen.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Text } from 'react-native'
 
+import DATA from '../data'
+import DetailScreen from './DetailScreen'
+
 const PostsScreen = ({ navigation }) => {
-    return <Text>Posts Screen</Text>
+    // return <Text>Posts Screen</Text>
+    return <DetailScreen post={DATA[0]} navigation={navigation} />
 }
 
 export default PostsScreen
