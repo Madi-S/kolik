7d55d8225c1aa63b1f0c11357c4a0353433dcc09
Madi-S <khovansky99@gmail.com>
2021-12-07 21:55:27 +0600

    Main Component style enhacements

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,31 +1,76 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native'
+import {
+    View,
+    ScrollView,
+    StyleSheet,
+    Dimensions,
+    ActivityIndicator
+} from 'react-native'
+import {
+    Text,
+    Card,
+    Icon,
+    Image,
+    Button,
+    ListItem
+} from 'react-native-elements'
+
+import { SCROLL_VIEW_MARGIN_BOTTOM } from '../config'
 
 const DetailScreen = ({ navigation, post }) => {
+    const screenWidth = Dimensions.get('window').width
+
     return (
-        <View>
+        <ScrollView style={{ marginBottom: SCROLL_VIEW_MARGIN_BOTTOM }}>
             <Button
                 title='Back to profile'
                 onPress={() => navigation.navigate('Profile')}
             />
-            <View>
-                <Text>Photo goes here {post.photo}</Text>
-            </View>
-            <View>
-                <Text>Title: {post.title}</Text>
-            </View>
-            <View>
+            <Card>
+                <Card.Title>{post.title}</Card.Title>
+                <Card.Divider />
+                <Card.Image
+                    source={{ uri: post.img }}
+                    style={{
+                        width: screenWidth,
+                        height: 200,
+                        marginBottom: 20
+                    }}
+                    PlaceholderContent={<ActivityIndicator />}
+                />
+                <Text>
+                    The idea with React Native Elements is more about component
+                    structure than actual design.
+                </Text>
                 <Text>Price: {post.price}</Text>
                 <Text>Phone number: {post.phoneNumber}</Text>
                 <Text>Category: {post.category}</Text>
                 <Text>Location: {post.location}</Text>
                 <Text>Created at: {Date(post.createdAt)}</Text>
-            </View>
-            <View>
-                <Text>Description: {post.description}</Text>
-            </View>
-        </View>
+                <Text style={{ marginBottom: 10 }}>
+                    Description: {post.description}
+                </Text>
+                <Button
+                    buttonStyle={{
+                        borderRadius: 0,
+                        marginLeft: 0,
+                        marginRight: 0,
+                        marginBottom: 0
+                    }}
+                    title='VIEW NOW'
+                />
+            </Card>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 20,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
 
 export default DetailScreen
