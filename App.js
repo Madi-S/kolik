1f315307d343cfa8f80dd10f5ba42b2daecf00ef
Madi-S <khovansky99@gmail.com>
2021-12-07 21:54:38 +0600

    Added theme provider

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
 import AppLoading from 'expo-app-loading'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
+import { ThemeProvider } from 'react-native-elements'
+import { useColorScheme } from 'react-native-appearance'
 import { NavigationContainer } from '@react-navigation/native'
 
 import Tabs from './src/tabs'
 import THEME from './src/theme'
 import loadApp from './src/loadApp'
 
-export default function App() {
+const providerTheme = {
+    Button: {
+        raised: true
+    }
+}
+
+const App = () => {
     console.log('Rendered app')
     const [isLoading, setIsLoading] = useState(true)
+
+    let colorScheme = useColorScheme()
+    let isDarkScheme = colorScheme === 'dark'
 
     if (isLoading) {
         return (
@@ -23,19 +34,16 @@ export default function App() {
     }
 
     return (
-        <>
+        <ThemeProvider theme={providerTheme} useDark={isDarkScheme}>
             <NavigationContainer>
                 <Tabs />
             </NavigationContainer>
             <StatusBar style='auto' />
-        </>
-        // <View style={styles.container}>
-        //     <Text style={styles.text}>Testing fonts</Text>
-        // </View>
+        </ThemeProvider>
     )
 }
 
-const styles = StyleSheet.create({
+const _styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: THEME.SECONDARY_COLOR,
@@ -47,3 +55,5 @@ const styles = StyleSheet.create({
         color: THEME.PRIMARY_COLOR
     }
 })
+
+export default App
