737df241f75aa27eaae22859a5c4445d19f24393
Madi-S <khovansky99@gmail.com>
2021-12-11 19:22:11 +0600

    PostsScreen posts display implemented via ListItem

--- src/screens/PostsScreen.js
+++ src/screens/PostsScreen.js
@@ -1,12 +1,31 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, ScrollView } from 'react-native'
+import { Card, ListItem } from 'react-native-elements'
 
 import DATA from '../data'
 import DetailScreen from './DetailScreen'
 
+import * as CONSTANTS from '../constants'
+
 const PostsScreen = ({ navigation }) => {
-    // return <Text>Posts Screen</Text>
-    return <DetailScreen post={DATA[0]} navigation={navigation} />
+    const posts = DATA
+
+    return (
+        <ScrollView
+            style={{
+                marginBottom: CONSTANTS.SCROLL_VIEW_MARGIN_BOTTOM,
+                fontfamily: 'm-bold'
+            }}
+        >
+            <Card containerStyle={{ padding: 0 }}>
+                {posts.map(post => (
+                    <ListItem key={post.id.toString()} post={post} />
+                ))}
+            </Card>
+        </ScrollView>
+    )
+
+    // return <DetailScreen post={DATA[0]} navigation={navigation} />
 }
 
 export default PostsScreen
