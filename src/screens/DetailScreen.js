55127d235424cfe54d4aaa80e19f8dc8a463ba21
Madi-S <khovansky99@gmail.com>
2021-12-11 19:22:27 +0600

    Show phone number logic implemented

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,49 +1,36 @@
-import React from 'react'
-import {
-    View,
-    ScrollView,
-    StyleSheet,
-    Dimensions,
-    ActivityIndicator
-} from 'react-native'
-import {
-    Text,
-    Card,
-    Icon,
-    Image,
-    Button,
-    ListItem
-} from 'react-native-elements'
+import React, { useState } from 'react'
+import { AntDesign } from '@expo/vector-icons'
+import { ScrollView, StyleSheet, Dimensions } from 'react-native'
+import { Text, Card, Button } from 'react-native-elements'
 
-import { SCROLL_VIEW_MARGIN_BOTTOM } from '../config'
+import * as CONSTANTS from '../constants'
 
 const DetailScreen = ({ navigation, post }) => {
+    const [phoneNumberIsShown, setPhoneNumberIsShown] = useState()
     const screenWidth = Dimensions.get('window').width
 
     return (
-        <ScrollView style={{ marginBottom: SCROLL_VIEW_MARGIN_BOTTOM }}>
-            <Button
-                title='Back to profile'
-                onPress={() => navigation.navigate('Profile')}
+        <ScrollView
+            style={{
+                marginBottom: CONSTANTS.SCROLL_VIEW_MARGIN_BOTTOM,
+                fontfamily: 'm-bold'
+            }}
+        >
+            <AntDesign
+                name='back'
+                size={24}
+                color='black'
+                // onPress={() => navigation.navigate('Profile')}
             />
             <Card>
                 <Card.Title>{post.title}</Card.Title>
                 <Card.Divider />
-                <Card.Image
-                    source={{ uri: post.img }}
-                    style={{
-                        width: screenWidth,
-                        height: 200,
-                        marginBottom: 20
-                    }}
-                    PlaceholderContent={<ActivityIndicator />}
-                />
+                <Card.Image source={{ uri: post.img }} />
                 <Text>
                     The idea with React Native Elements is more about component
                     structure than actual design.
                 </Text>
                 <Text>Price: {post.price}</Text>
-                <Text>Phone number: {post.phoneNumber}</Text>
                 <Text>Category: {post.category}</Text>
                 <Text>Location: {post.location}</Text>
                 <Text>Created at: {Date(post.createdAt)}</Text>
@@ -57,8 +44,20 @@ const DetailScreen = ({ navigation, post }) => {
                         marginRight: 0,
                         marginBottom: 0
                     }}
-                    title='VIEW NOW'
+                    onClick={() => {
+                        phoneNumberIsShown
+                            ? setPhoneNumberIsShown(false)
+                            : setPhoneNumberIsShown(true)
+                    }}
+                    title={
+                        phoneNumberIsShown
+                            ? 'SHOW PHONE NUMBER'
+                            : 'HIDE PHONE NUMBER'
+                    }
                 />
+                {phoneNumberIsShown ? (
+                    <Text>Phone number: {post.phoneNumber}</Text>
+                ) : null}
             </Card>
         </ScrollView>
     )
