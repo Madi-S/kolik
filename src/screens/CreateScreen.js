9ffba1178966df95f770c972c13815067b9e6c6c
Madi-S <khovansky99@gmail.com>
2021-12-27 14:37:41 +0600

    CreateScreen filled with components and logic

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,8 +1,81 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+
+import PhotoPicker from '../components/PhotoPicker'
+import { AppButton } from '../components/core/button'
+import { AppInput } from '../components/core/input'
+import { AppSelect } from '../components/core/select'
+import { AppTextArea } from '../components/core/textarea'
+
+import { LOCATIONS, CATEGORIES } from '../data'
+import * as CONSTANTS from '../constants'
+
+const DEFAULT_LOCATION = LOCATIONS[0].value
+const DEFAULT_CATEGORY = CATEGORIES[0].value
 
 const CreateScreen = ({ navigation }) => {
-    return <Text>Create Screen</Text>
+    const [title, setTitle] = useState('')
+    const [phone, setPhone] = useState('+7')
+    const [price, setPrice] = useState(0)
+    const [location, setLocation] = useState(DEFAULT_LOCATION)
+    const [category, setCategory] = useState(DEFAULT_CATEGORY)
+    const [description, setDescription] = useState('')
+
+    const processImage = img => {
+        console.log(img)
+    }
+
+    const createPost = () => {
+        const post = { title, phone, price, location, category, description }
+        console.log('Creating post with data: ', post)
+    }
+
+    return (
+        <View>
+            <Text>Create own Car Post</Text>
+            <ScrollView>
+                <PhotoPicker onPick={processImage} />
+                <AppInput title='Title' value={title} onChangeText={setTitle} />
+                <AppInput
+                    title='Contact phone number'
+                    keyboardType='numeric'
+                    value={phone}
+                    onChangeText={setPhone}
+                />
+                <AppInput
+                    title='Price'
+                    keyboardType='numeric'
+                    value={price}
+                    onChangeText={setPrice}
+                />
+                <AppTextArea
+                    title='Description'
+                    value={description}
+                    onChangeText={setDescription}
+                />
+                <AppSelect
+                    title='Location'
+                    selectedValue={location}
+                    onValueChange={setLocation}
+                    itemsList={LOCATIONS}
+                />
+                <AppSelect
+                    title='Location'
+                    selectedValue={category}
+                    onValueChange={setCategory}
+                    itemsList={CATEGORIES}
+                />
+
+                <AppButton onPress={createPost} style={styles.createButton} />
+            </ScrollView>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    createButton: {
+        marginBottom: CONSTANTS.SCROLL_VIEW_MARGIN_BOTTOM
+    }
+})
 
 export default CreateScreen
